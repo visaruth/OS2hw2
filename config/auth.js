20cde549d71f36bfc48d71e2ccff20d702ae82a2
visaruth <megasama.one1@gmail.com>
2016-10-24 11:44:08 +0700

    summit

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'facebookAuth': {
+        'clientID': '261234257580110', // your App ID
+        'clientSecret': 'b566b42624e2c9e06017e560761437d1', // your App Secret
+        'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth': {
+        'consumerKey': 'AdrFtKpjbWHBL6v70UJ3mpvj5',
+        'consumerSecret': 'Elmiti3U5ybQgczEi0mpqCPTP1YpO33AngsPexq8ECgyNNE2JQ',
+        'callbackURL': 'http://127.0.0.1:8080/auth/twitter/callback'
+    },
+
+    'googleAuth': {
+        'clientID': '931720492856-gnvsco46t8rrpujnmr3b2p44rqso915i.apps.googleusercontent.com',
+        'clientSecret': 'se5Fc_YfhcXKYG3Ixpg0AdBH',
+        'callbackURL': 'http://127.0.0.1:8080/auth/google/callback'
+    }
+
+};
